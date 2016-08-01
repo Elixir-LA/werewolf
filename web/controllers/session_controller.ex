d5537c1d8411f1bb141422686477535cdbdf6394
Andrew Hao <andrewhao@gmail.com>
2016-08-01 14:47:42 -0700

    Adding SessionController and tests.

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -3,17 +3,17 @@ defmodule Werewolf.SessionController do
 
   alias Werewolf.Game
 
-  def create(conn, %{"game" => %{"slug" => slug, "name" => name}}) do
-    # changeset = Game.changeset(%Game{slug: generate_slug})
-
-    # case Repo.insert(changeset) do
-    #   {:ok, game} ->
+  def create(conn, %{"game" => %{"name" => user_name, "slug" => slug}}) do
+    case Repo.get_by(Game, slug: slug) do
+      nil ->
         conn
-        |> put_flash(:info, "Game joined successfully.")
-        |> put_session(:user_name, name)
-        |> redirect(to: game_path(conn, :show, slug))
-      # {:error, changeset} ->
-      #   render(conn, "new.html", changeset: changeset)
-    # end
+        |> put_flash(:error, "Sorry, I can't find that game!")
+        |> redirect(to: page_path(conn, :index))
+      game ->
+        conn
+        |> put_flash(:info, "You have joined, #{user_name}!")
+        |> put_session(:user_name, user_name)
+        |> redirect(to: game_path(conn, :show, game.slug))
+    end
   end
 end
