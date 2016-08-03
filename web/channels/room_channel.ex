69863d6feb3b6d4f0fd09928821c27f44d78f13d
hannahhoward <hannah@hannahhoward.net>
2016-08-03 16:51:48 -0700

    Track presence on websocket join

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,7 +1,9 @@
 defmodule Werewolf.RoomChannel do
   use Werewolf.Web, :channel
+  alias Werewolf.Presence
 
   def join(_room, _payload, socket) do
+    send(self, :after_join)
     {:ok, socket}
   end
 
@@ -11,6 +13,14 @@ defmodule Werewolf.RoomChannel do
       body: message,
       timestamp: :os.system_time(:milli_seconds)
     }
+    {:noreply, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    {:ok, _} = Presence.track(socket, socket.assigns.user, %{
+      online_at: :os.system_time(:milli_seconds)
+    })
+    push socket, "presence_state", Presence.list(socket)
     {:noreply, socket}
   end
 
