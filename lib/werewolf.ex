7dcec833160d96489fe97ff9a25fb17887873869
Andrew Hao <andrewhao@gmail.com>
2016-09-05 13:46:36 -0700

    Adding a Gameplay.Supervisor to manage Gameplay processes.

--- lib/werewolf.ex
+++ lib/werewolf.ex
@@ -11,9 +11,9 @@ defmodule Werewolf do
       supervisor(Werewolf.Endpoint, []),
       # Start the Ecto repository
       supervisor(Werewolf.Repo, []),
-
       supervisor(Werewolf.Presence, []),
-      
+      supervisor(Werewolf.Gameplay.Supervisor, []),
+
       # Here you could define other workers and supervisors as children
       # worker(Werewolf.Worker, [arg1, arg2, arg3]),
     ]
