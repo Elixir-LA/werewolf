0c497b08f297aa0a3262b1df2e0f2834114462f5
Andrew Hao <andrew@carbonfive.com>
2016-09-07 09:30:33 -0700

    Adding a GameplayTest

---
+++ test/lib/gameplay_test.exs
@@ -0,0 +1,9 @@
+defmodule Werewolf.GameplayTest do
+  use ExUnit.Case, async: true
+  alias Werewolf.Gameplay
+
+  test 'joining a game that does not exist' do
+    fake_pid = :c.pid(0,100,0)
+    assert {:error, "Game does not exist"} = Gameplay.join('game-1', 'hubert', fake_pid)
+  end
+end
